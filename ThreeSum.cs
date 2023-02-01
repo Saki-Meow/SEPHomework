b3b0d581135e4fdd941ba3836167ba53f78afc80
Claudia Feng <71486027+Saki-Meow@users.noreply.github.com>
2023-02-01 13:01:14 -0500

    Add files via upload

---
+++ ThreeSum.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jan2023.Console.LeetCode
+{
+    public class ThreeSum
+    {
+        public static void main()
+        {
+
+        }
+
+        public static IList<IList<int>> Solution(int[] nums)
+        {
+            List<IList<int>> result = new List<IList<int>>();
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+                int left = i + 1, right = nums.Length - 1;
+                while (left < right)
+                {
+                    int sum = nums[i] + nums[left] + nums[right];
+                    if (sum == 0)
+                    {
+                        result.Add(new List<int> { nums[i], nums[left], nums[right] });
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        left++;
+                        right--;
+                    }
+                    else if (sum < 0)
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ TwoSum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jan2023.Console.LeetCode
+
+{
+    using System;
+    public class TwoSum
+    {
+        public static void Main() 
+        {
+            int[] ints = Solution(new int[] {2, 7, 11, 15}, 9);
+            Console.WriteLine(ints);
+        }
+
+        public static int[] Solution(int[] nums, int target)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (map.ContainsKey(target - nums[i]))
+                {
+                    int prevIndex = map[target - nums[i]];
+                    return new int[] {prevIndex, i};
+                } else
+                {
+                    map[nums[i]] = i;
+                }
+            }
+            return null;
+        }
+    }
+}
